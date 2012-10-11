f919fe5bb113ed6cad754e1abfe12f2662977e6e
Victor Quinn <vquinn@ngpvan.com>
2012-10-11 17:35:10 -0400

    Renamed default.config.rb to default.config.yaml, added a silly trivial test

--- default.config.rb
+++
@@ -1,1 +0,0 @@
-:app_host = "http://victorquinn.com"
--- spec/drupal_capybara_spec.rb
+++ spec/drupal_capybara_spec.rb
@@ -2,12 +2,20 @@ require 'rspec'
 require 'capybara'
 require 'capybara/dsl'
 require 'spec_helper.rb'
-require '../config.rb'
+require 'yaml'
+
+# First load the config file and all its attributes
+yaml = YAML::load(File.expand_path("../config.yaml"))
 
 Capybara.default_driver = :selenium
-Capybara.app_host = :app_host
+Capybara.app_host = yaml['app_host']
 
 describe "Searching for a search box" do
   it "allows searching for words" do
+    starts = Time.now
+    visit "http://www.google.com"
+    fill_in('q', :with => 'banana')
+    page.should have_content("Banana")
+    p Time.now - starts
   end
 end
