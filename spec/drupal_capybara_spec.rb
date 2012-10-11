d2776173984c03749a8c6f9c81f0d55536ec5179
Victor Quinn <vquinn@ngpvan.com>
2012-10-11 18:31:14 -0400

    Now using Capybara webkit

--- spec/drupal_capybara_spec.rb
+++ spec/drupal_capybara_spec.rb
@@ -1,21 +1,22 @@
 require 'rspec'
 require 'capybara'
 require 'capybara/dsl'
+require 'capybara/webkit'
 require 'spec_helper.rb'
+#require 'test_helper.rb'
 require 'yaml'
 
 # First load the config file and all its attributes
 yaml = YAML::load(File.expand_path("../config.yaml"))
+site = yaml['site']
 
-Capybara.default_driver = :selenium
-Capybara.app_host = yaml['app_host']
+Capybara.default_driver = :webkit
+Capybara.javascript_driver = :webkit
+Capybara.app_host = site
 
-describe "Searching for a search box" do
-  it "allows searching for words" do
-    starts = Time.now
-    visit "http://www.google.com"
-    fill_in('q', :with => 'banana')
-    page.should have_content("Banana")
-    p Time.now - starts
+describe "Testing VictorQuinn.com" do
+  it "page can be hit" do
+    visit "http://victorquinn.com"
+    page.should have_content("Victor")
   end
 end
