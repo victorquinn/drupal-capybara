b33d45dac71141ecd5ea51aaac5fdf60a86592d3
Victor Quinn <vquinn@ngpvan.com>
2012-10-11 16:56:25 -0400

    Added default.config.rb, loading it and using :app_host from it rather than hard coding in other code.

--- drupal_capybara_spec.rb
+++ drupal_capybara_spec.rb
@@ -2,6 +2,12 @@ require 'rspec'
 require 'capybara'
 require 'capybara/dsl'
 require 'spec_helper.rb'
+require 'config.rb'
 
 Capybara.default_driver = :selenium
-Capybara.app_host = "http://www.google.com"
+Capybara.app_host = :app_host
+
+describe "Searching for a search box" do
+  it "allows searching for words" do
+  end
+end
